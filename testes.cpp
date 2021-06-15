016bdbb918042cfc2ab60d64563ba7050eb0daff
DanLoose <danloose14@gmail.com>
2021-06-15 15:33:51 -0300

    reformulação do codigo para map<chave<vec>>

--- testes.cpp
+++ testes.cpp
@@ -17,51 +17,35 @@ struct cotacao
   int valor;
 };
 
-map<string, map<string, vector<cotacao>>> myMap;
+map<string, vector<cotacao>> historicoBovespa;
 
-void addData(string Date1, string Date2, string V1, int V2)
+void addCotacao(string sigla,  string data, int valor)
 {
-  cotacao myData;
-  myData.data = V1;
-  myData.valor = V2;
-  myMap[Date1][Date2].push_back(myData);
+  cotacao minhaCotacao;
+  minhaCotacao.data = data;
+  minhaCotacao.valor = valor;
+  historicoBovespa[sigla].push_back(minhaCotacao);
 }
 
 int main()
 {
-  int iTotal = 0;
 
-  addData("vale", "Blah2", "ontem", 1);
-  addData("vale", "Blah2", "ontem", 1);
-  addData("brad", "Blah1", "amanha", 1);
-  addData("brad", "Blah3", "hoje", 1);
+  addCotacao("vale", "ontem", 1);
+  addCotacao("vale", "ontem", 1);
+  addCotacao("vale", "ontem", 1);
+  addCotacao("brad", "amanha", 1);
+  addCotacao("brad", "hoje", 1);
 
-  cout << "There Are " << myMap.size() << " different Date1's" << endl;
+  cout << "existem " << historicoBovespa.size() << " acoes diferentes" << endl;
 
-  // Loop Through Each Date1, Count How many Date2's Each Date1 Has.
-  map<string, map<string, vector<cotacao>>>::iterator vPtr = myMap.begin();
-  while (vPtr != myMap.end())
+  // loop que retorna quantos elementos tem cada chave
+  map<string, vector<cotacao>>::iterator vPtr = historicoBovespa.begin();
+  while (vPtr != historicoBovespa.end())
   {
-    cout << "Date1: " << (*vPtr).first << " has " << myMap[(*vPtr).first].size() << " different Date2s" << endl;
-    iTotal += myMap[(*vPtr).first].size();
+    cout << "acao: " << (*vPtr).first << " possui " << (*vPtr).second.size() << " registros" << endl;
     vPtr++;
   }
-  cout << "There are a total of " << iTotal << " dDate2's across your Date1's" << endl;
 
-  cout << "Printing Pairs: " << endl;
-  // Loop Through And List of All Date2s
-  vPtr = myMap.begin();
-  while (vPtr != myMap.end())
-  {
-    // Create Date2 Iterator
-    map<string, vector<cotacao>>::iterator v2Ptr = myMap[(*vPtr).first].begin();
-    while (v2Ptr != myMap[(*vPtr).first].end())
-    {
-      cout << "Pair: " << (*vPtr).first << " : " << (*v2Ptr).first << endl;
-      v2Ptr++;
-    }
-    vPtr++;
-  }
 
   return 0;
 }

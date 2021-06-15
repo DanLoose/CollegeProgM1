cadb8bc1d7a30f40a87bc87545cf2e1db9418787
DanLoose <danloose14@gmail.com>
2021-06-15 15:11:51 -0300

    criei o struct cotacao e substitui no codigo

--- testes.cpp
+++ testes.cpp
@@ -5,39 +5,43 @@
 
 using namespace std;
 
-struct Data {
+struct Data
+{
   int Value1;
   int Value2;
 };
 
-struct cotacao{
-    string data;
-    int valor;
+struct cotacao
+{
+  string data;
+  int valor;
 };
 
-map<string, map<string, vector<Data> > > myMap;
+map<string, map<string, vector<cotacao>>> myMap;
 
-void addData(string Date1, string Date2, int V1, int V2) {
-  Data myData;
-  myData.Value1 = V1;
-  myData.Value2 = V2;
+void addData(string Date1, string Date2, string V1, int V2)
+{
+  cotacao myData;
+  myData.data = V1;
+  myData.valor = V2;
   myMap[Date1][Date2].push_back(myData);
 }
 
-
-int main() {
+int main()
+{
   int iTotal = 0;
 
-  addData("vale", "Blah2", 1, 1);
-  addData("vale", "Blah2", 1, 1);
-  addData("brad", "Blah1", 1, 1);
-  addData("brad", "Blah3", 1, 1);
+  addData("vale", "Blah2", "ontem", 1);
+  addData("vale", "Blah2", "ontem", 1);
+  addData("brad", "Blah1", "amanha", 1);
+  addData("brad", "Blah3", "hoje", 1);
 
   cout << "There Are " << myMap.size() << " different Date1's" << endl;
 
   // Loop Through Each Date1, Count How many Date2's Each Date1 Has.
-  map<string, map<string, vector<Data> > >::iterator vPtr = myMap.begin();
-  while (vPtr != myMap.end()) {
+  map<string, map<string, vector<cotacao>>>::iterator vPtr = myMap.begin();
+  while (vPtr != myMap.end())
+  {
     cout << "Date1: " << (*vPtr).first << " has " << myMap[(*vPtr).first].size() << " different Date2s" << endl;
     iTotal += myMap[(*vPtr).first].size();
     vPtr++;
@@ -47,10 +51,12 @@ int main() {
   cout << "Printing Pairs: " << endl;
   // Loop Through And List of All Date2s
   vPtr = myMap.begin();
-  while (vPtr != myMap.end()) {
+  while (vPtr != myMap.end())
+  {
     // Create Date2 Iterator
-    map<string, vector<Data> >::iterator v2Ptr = myMap[(*vPtr).first].begin();
-    while (v2Ptr != myMap[(*vPtr).first].end()) {
+    map<string, vector<cotacao>>::iterator v2Ptr = myMap[(*vPtr).first].begin();
+    while (v2Ptr != myMap[(*vPtr).first].end())
+    {
       cout << "Pair: " << (*vPtr).first << " : " << (*v2Ptr).first << endl;
       v2Ptr++;
     }
